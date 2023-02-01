1f9f185b68b67334360b528a09eeedf3ccaf7f07
araf821 <araf.ahmed200@gmail.com>
2023-01-31 22:23:03 -0500

    liker bot completed

--- liker_bot.py
+++ liker_bot.py
@@ -23,21 +23,24 @@ input_password.send_keys(password)
 input_password.send_keys(Keys.ENTER)
 sleep(3)
 
-victim_link = "https://www.instagram.com/georgeson/"
+# Provide a link to the whoever you want to invade
+victim_link = "https://www.instagram.com/"
 driver.get(victim_link)
 sleep(3)
 
+# Get the <a> tag that contains a link to the standalone post for all posts
 posts = driver.find_elements(
-    By.CSS_SELECTOR, '._aang > ._aabd > a')
+    By.CSS_SELECTOR, 'article div div div div a')
 
-print(posts)
-for post in posts:
-    post.click()
-    print("Clicked on post")
-    sleep(2)
-    post_content = driver.find_element(
-        By.XPATH, '//*[@id="mount_0_0_pd"]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button')
-    action.double_click(post_content)
-    sleep(0.5)
-    action.send_keys(Keys.ESCAPE)
-    sleep(0.5)
+def like_posts():
+    for link in post_links:
+        driver.get(link)
+        sleep(3)
+        like_btn = driver.find_element(
+            By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button')
+        like_btn.click()
+        sleep(0.5)
+
+# Seperate the link from the <a> tag for all posts
+post_links = [post.get_attribute("href") for post in posts]
+like_posts()
